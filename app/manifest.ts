dd218d4e74141e4cddeff0fa4ecc104662a1dae9
PolCarva <pablocarvalhogimenez@gmail.com>
2024-06-04 13:16:21 -0300

    First commit

---
+++ app/manifest.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Ns Group | Demoscpía, Eyewatch, Illuminati Lab y Nicestream",
+    short_name: "Ns Group",
+    description:
+      "Somos un ecosistema de comunicación estratégica especializado en asuntos empresariales, inteligencia electoral y public affairs.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      }
+    ],
+  };
+}
