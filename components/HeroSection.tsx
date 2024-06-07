b3381d0426134d4840e677d16ccdedb3fadd0f7c
PolCarva <pablocarvalhogimenez@gmail.com>
2024-06-07 14:37:04 -0300

    Fix medium devices hero styling

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -11,26 +11,26 @@ const HeroSection = () => {
       <section className="h-[80svh] pt-20 md:pt-0 flex items-center">
         <div className="mx-auto w-full flex flex-col justify-center gap-20 md:pl-0 relative bg-primary">
           <h1
-            className={`uppercase leading-normal md:leading-none relative tracking-wider text-[3.9rem] lg:text-[10rem] ${durk.className}`}
+            className={`uppercase leading-normal md:leading-none relative tracking-wider text-[3.9rem] md:text-9xl lg:text-[10rem] ${durk.className}`}
           >
             <Tag text="Especialistas en" className="absolute bottom-full left-0" />
-            <span className="bg-white scale-105 pl-[5%] md:pl-0 md:scale-100 w-full md:w-fit pr-5 block">
+            <span className="bg-white scale-105 pl-[5%] lg:pl-0 lg:scale-100 w-full lg:w-fit pr-5 block">
               Social Science
             </span>
-            <span className="bg-white scale-105 md:scale-100 pl-[5%] md:pl-0 w-full md:w-fit pr-5 block">
-              Comunicación <span className="hidden md:inline">&</span>
+            <span className="bg-white scale-105 lg:scale-100 pl-[5%] lg:pl-0 w-full lg:w-fit pr-5 block">
+              Comunicación <span className="hidden lg:inline">&</span>
             </span>
 
-            <span className="bg-white scale-105 pl-[4%] md:pl-0 md:scale-100 w-fit pr-5 block">
+            <span className="bg-white scale-105 pl-[4%] lg:pl-0 lg:scale-100 w-fit pr-5 block">
               Big Data
             </span>
           </h1>
           <Link
             href={"#contacto"}
             scroll
-            className="w-full py-5 md:w-fit md:px-[9.6%] grid group place-content-center h-1/3 text-white md:absolute bottom-0 right-0 bg-black"
+            className="w-full py-5 md:self-end md:px-[12%] md:py-8 md:w-fit lg:px-[8.75%] grid group place-content-center h-1/3 text-white lg:absolute bottom-0 right-0 bg-black"
           >
-            <span className="capitalize group-hover:w-[110%] transition text-xl relative">
+            <span className="capitalize group-hover:w-[110%] transition text-2xl relative">
               Contáctanos
             </span>
             <div className="w-full h-px mt-2 relative bg-white  lg:group-hover:scale-x-125 transition-transform ease-in-out duration-300">
